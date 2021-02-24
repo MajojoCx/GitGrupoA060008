c33158f9c62f21338c7fd77608ae0e13703b6e4e
MR GB <MR GB@LAPTOP-QJF7050U>
2021-02-23 21:20:53 -0600

    :D

---
+++ HitA060008/src/hita060008/HitA060008.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hita060008;
+
+/**
+ *
+ * @author MR GB
+ */
+public class HitA060008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("mi nombre es Maria");
+    }
+    
+}
